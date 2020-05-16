5de01f25bb64ee169c5265ec5b0ddd5b154ad222
Mario Domenech Goulart <mario@parenteses.org>
2020-05-16 14:11:28 +0200

    pipe-smoker-cmd: use optimism and handle help options

--- pipe-smoker-cmd.scm
+++ pipe-smoker-cmd.scm
@@ -9,9 +9,10 @@
         (chicken pathname)
         (chicken port)
         (chicken process-context)
+        (chicken sort)
         (chicken string))
 (import pipe-smoker)
-(import srfi-1 srfi-13 slice)
+(import optimism srfi-1 srfi-13 slice)
 
 (include "commands/cols.scm")
 (include "commands/format.scm")
@@ -25,10 +26,35 @@
   (when (file-exists? user-conf)
     (load user-conf)))
 
-(let ((args (command-line-arguments)))
-  (or (and-let* ((cmd (string->symbol (car args)))
+(define (usage #!optional exit-code)
+  (let ((out (if (and exit-code (not (zero? exit-code)))
+                 (current-error-port)
+                 (current-output-port))))
+    (fprintf out "Usage: ~a [-h|-help|--help] <command> [<command args>]\n"
+             (pathname-file (program-name)))
+    (fprintf out "\n<command>s are:\n")
+    (for-each (lambda (command)
+                (fprintf out "~a\n" command))
+              (sort (map (compose symbol->string car) (get-commands))
+                     string<))
+    (when exit-code
+      (exit exit-code))))
+
+(let* ((args (parse-command-line
+              (command-line-arguments)
+              `(((-h -help --help))
+                (--))))
+       (command (alist-ref '-- args)))
+
+  (when (alist-ref '-h args)
+    (usage 0))
+
+  (when (null? command)
+    (usage 1))
+
+  (or (and-let* ((cmd (string->symbol (car command)))
                  (handler (alist-ref cmd (get-commands))))
-        (apply (command-proc handler) (cdr args)))
-      (error "Invalid command: " (car args))))
+        (apply (command-proc handler) (cdr command)))
+      (die! "Invalid command: ~a" (car command))))
 
 ) ;; end module
