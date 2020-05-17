d3baa61e60ed55134ce0d3c6dec38eb4fbc1be5c
Mario Domenech Goulart <mario@parenteses.org>
2020-05-17 12:59:22 +0200

    filter: add --stop-after-matches parameter

--- commands/filter.scm
+++ commands/filter.scm
@@ -9,33 +9,52 @@ filter [<options>] <pattern>
 
   --sre | -S
     Indicate that <pattern> uses SRE syntax.
+
+  --stop-after-matches | -n <num matches>
+    Stop after reaching any matches.
 "
   (lambda args*
     (let* ((args (parse-command-line
                   args*
-                  '(((--help -help -h))
+                  `(((--help -help -h))
                     ((--read-sexp -r))
+                    ((--stop-after-matches -n) . matches)
                     ((--sre -S)))))
            (read-sexp? (get-opt '(--read-sexp -r) args flag?: #t))
            (use-sre? (get-opt '(--sre -S) args flag?: #t))
+           (stop-after-matches (get-opt '(--stop-after-matches -n) args))
            (pattern (and-let* ((p (get-opt '(--) args)))
                       (and (not (null? p)) (car p)))))
 
       (handle-command-help 'filter args)
 
+      (when stop-after-matches
+        (let ((n (string->number stop-after-matches)))
+          (if n
+              (set! stop-after-matches n)
+              (die! "filter: --stop-after-matches | -n: invalid argument: ~a."
+                    stop-after-matches))))
+
       (unless pattern
         (die! "filter: missing pattern."))
 
-      (input-iterator
-       read-sexp?
-       (lambda (sexp)
-         (when (eval `(let ((INPUT ,sexp))
-                        (begin ,@(with-input-from-string pattern read-list))))
-           (print sexp)))
+      (let ((matches -1))
+        (input-iterator
+         read-sexp?
+         (lambda (sexp)
+           (when (eval `(let ((INPUT ,sexp))
+                          (begin ,@(with-input-from-string pattern read-list))))
+             (set! matches (add1 matches))
+             (when (and stop-after-matches (= matches stop-after-matches))
+               (exit 0))
+             (print sexp)))
        (lambda (line)
          (when (irregex-search
                 (if use-sre?
                     (with-input-from-string pattern read)
                     pattern)
                 line)
-           (print line)))))))
+           (set! matches (add1 matches))
+           (when (and stop-after-matches (>= matches stop-after-matches))
+             (exit 0))
+           (print line))))))))
