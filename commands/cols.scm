1d8d40303588c32efb5f1fad93cb122bea214d5e
Mario Domenech Goulart <mario@parenteses.org>
2020-05-18 20:39:41 +0200

    cols: add --pattern and --sre options

--- commands/cols.scm
+++ commands/cols.scm
@@ -1,7 +1,10 @@
 (define-command 'cols
   "\
 cols [<options>] <range> [<range> ...]
-  Select columns based on ranges.  Syntax of ranges:
+  Select columns based on ranges.  Columns are split on spaces and tabs
+  by default.
+
+  Syntax of ranges:
   * `<number>': a single column whose index is <number>
   * `:<number>': columns from 0 to <number>
   * `<number>:': columns from <number> to the last column
@@ -10,6 +13,13 @@ cols [<options>] <range> [<range> ...]
   <options>:
     --sep | -s
       Characters used to separate tokens (given to `string-split').
+
+    --pattern | -p
+      Regular expression to be used to split columns.  --pattern and --sep
+      are mutually exclusive.
+
+    --sre | -S
+      Indicate that the argument to --pattern is an SRE.
 
     --read-sexp | -r
       Assume inputs are sexps.
@@ -21,26 +31,44 @@ cols [<options>] <range> [<range> ...]
     (let* ((args (parse-command-line
                   args*
                   '(((--help -help -h))
+                    ((--pattern -p) . pattern)
+                    ((--sre -S))
                     ((--read-sexp -r))
                     ((--write-sexp -w))
                     ((--sep -s) . separator)
                     )))
            (read-sexp? (get-opt '(--read-sexp -r) args flag?: #t))
            (write-sexp? (get-opt '(--write-sexp -w) args flag?: #t))
-           (sep (or (get-opt '(--sep -s) args) " \t"))
-           (ranges (parse-ranges 'cols (get-opt '(--) args))))
+           (sep (get-opt '(--sep -s) args))
+           (pattern (get-opt '(--pattern -p) args))
+           (use-sre? (get-opt '(--sre -S) args flag?: #t))
+           (ranges (parse-ranges 'cols (get-opt '(--) args)))
+           (default-sep " \t"))
 
       (handle-command-help 'cols args)
 
       (when (null? ranges)
-        (die! "cols: missing columns specification"))
+        (die! "cols: missing columns specification."))
+
+      (when (and sep pattern)
+        (die! "cols: --sep and --pattern are mutually exclusive."))
+
+      (when (and use-sre? (not pattern))
+        (die! "cols: --sre requires --pattern."))
 
       (let ((iterator (if read-sexp? for-each-sexp for-each-line)))
         (iterator
          (lambda (line-or-sexp lineno)
-           (let* ((items (if read-sexp?
-                             line-or-sexp
-                             (string-split line-or-sexp sep)))
+           (let* ((items
+                   (if read-sexp?
+                       line-or-sexp
+                       (if pattern
+                           (irregex-split
+                            (if use-sre?
+                                (with-input-from-string pattern read)
+                                pattern)
+                            line-or-sexp)
+                           (string-split line-or-sexp (or sep default-sep)))))
                   (slices
                    (map (lambda (range)
                           (apply list-slice (list items range)))
