5f288934e34c986ecb5feac6285db03eb2f578e6
Mario Domenech Goulart <mario@parenteses.org>
2020-05-24 21:30:20 +0200

    eval: add --match and --not-match command line parameters

--- commands/eval.scm
+++ commands/eval.scm
@@ -16,7 +16,7 @@ eval <options> <exp>
       Regular expression to be used to split columns.
 
     --sre | -S
-      Indicate that the argument for --split-pattern is an SRE.
+      Indicate that regular expressions are in SRE syntax.
 
     --read-sexp | -r
       Assume inputs are sexps.
@@ -28,6 +28,16 @@ eval <options> <exp>
     --finalizer | -f <exp>
       Scheme expression to be evaluated after the whole input has been
       consumed.
+
+    --match | -m <pattern>
+      Only apply <exp> to lines which match <pattern> (a regular
+      expression).  Lines that do not match <pattern> are just printed.
+      --match and --read-sexp are mutually exclusive.
+
+    --not-match | -n <pattern>
+      Only apply <exp> to lines which do NOT match <pattern> (a regular
+      expression).  Lines that match <pattern> are just printed.
+      --not-match and --read-sexp are mutually exclusive.
 "
   (lambda args*
     (let* ((args (parse-command-line
@@ -40,6 +50,8 @@ eval <options> <exp>
                     ((--finalizer -f) . finalizer)
                     ((--read-sexp -r))
                     ((--sre -S))
+                    ((--match -m) . p)
+                    ((--not-match -n) . p)
                     ((--split-pattern -s) . pattern)
                     )))
            (read-sexp? (get-opt '(--read-sexp -r) args flag?: #t))
@@ -47,7 +59,19 @@ eval <options> <exp>
            (extensions
             (or (get-opt '(--require-extension -R) args multiple?: #t) '()))
            (finalizer (get-opt '(--finalizer -f) args))
+           (match-pattern% (get-opt '(--match -m) args))
+           (not-match-pattern% (get-opt '(--not-match -n) args))
            (use-sre? (get-opt '(--sre -S) args flag?: #t))
+           (match-pattern
+            (and match-pattern%
+                 (if use-sre?
+                     (with-input-from-string match-pattern% read)
+                     (string->sre match-pattern%))))
+           (not-match-pattern
+            (and not-match-pattern%
+                 (if use-sre?
+                     (with-input-from-string not-match-pattern% read)
+                     (string->sre not-match-pattern%))))
            (split-pattern (get-opt '(--split-pattern -s) args))
            (exp (and-let* ((e (get-opt '(--) args)))
                   (and (not (null? e)) (car e)))))
@@ -57,6 +81,12 @@ eval <options> <exp>
       (unless exp
         (die! "eval: missing expression"))
 
+      (when (and match-pattern read-sexp?)
+        (die! "--match and --read-sexp are mutually exclusive."))
+
+      (when (and not-match-pattern read-sexp?)
+        (die! "--not-match and --read-sexp are mutually exclusive."))
+
       (let ((iterator (if read-sexp? for-each-sexp for-each-line))
             (pattern (and split-pattern
                           (if use-sre?
@@ -64,9 +94,14 @@ eval <options> <exp>
                               (string->sre split-pattern)))))
         (iterator
          (lambda (line-or-sexp lineno)
-           (set! bindings
-                 (cdr (eval-scheme
-                       exp bindings extensions line-or-sexp lineno pattern))))
+           (if (or (and (not match-pattern) (not not-match-pattern))
+                   (and match-pattern (irregex-search match-pattern line-or-sexp))
+                   (and not-match-pattern
+                        (not (irregex-search not-match-pattern line-or-sexp))))
+               (set! bindings
+                     (cdr (eval-scheme
+                           exp bindings extensions line-or-sexp lineno pattern)))
+               (print line-or-sexp)))
          finalizer: (and finalizer
                          (lambda (lineno)
                            (eval-scheme
