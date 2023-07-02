908a32236ae92596468d1dd3b0ae9093f2443f4c
Mario Domenech Goulart <mario@parenteses.org>
2023-07-02 10:54:31 +0200

    Add description of sad to help message

--- sad-cmd.scm
+++ sad-cmd.scm
@@ -26,6 +26,24 @@
 (include "commands/split.scm")
 (include "commands/join.scm")
 
+(define sad-message "
+sad -- Scheme-Aware Ducts (\"sed makes me sad\").
+
+sad is a program for text processing on the command line.  It
+provides limited commands for usual and specific tasks and more
+powerful ones for more elaborated tasks.
+
+All sad commands read from the standard input.
+
+Most sad commands have the ability to read Scheme lists as inputs
+and write them as outputs, as a way to provide a better protocol
+between sad commands.
+
+Some commands accept regular expressions as inputs.  In those cases,
+commands provide options to specify them as strings or as _Extended
+Scheme Regular Expression Syntax_ (i.e., the one used by irregex).
+")
+
 (let ((user-conf
        (make-pathname (get-environment-variable "HOME") ".sad.conf")))
   (when (file-exists? user-conf)
@@ -34,10 +52,10 @@
 (let ((args (command-line-arguments)))
 
   (when (null? args)
-    (show-main-help 1))
+    (show-main-help 1 message: sad-message))
 
   (when (member (car args) '("-h" "-help" "--help"))
-    (show-main-help 0))
+    (show-main-help 0 message: sad-message))
 
   (or (and-let* ((cmd (string->symbol (car args)))
                  (handler (alist-ref cmd (commands))))
