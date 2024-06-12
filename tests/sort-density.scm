a7d0bcd082827b8d70acd1c332d8310a5727f016
Mario Domenech Goulart <mario@parenteses.org>
2024-06-12 07:41:04 +0200

    sort-density: Swap arguments of head
    Suggested by Kon Lovett.

--- tests/sort-density.scm
+++ tests/sort-density.scm
@@ -2,22 +2,22 @@
           "cat density.txt | sad lines :2;\
            cat density.txt | sad lines 2: | sad split '|' | sad buffer -r |\
            sad sort -C numeric -c 0 | sad format '~a|~9@a|~12@a|~11@a|~22@a~%'"
-          "head density.txt -n2 ; tail -10 density.txt | sort -n -t '|' -k 1")
+          "head -n2 density.txt ; tail -10 density.txt | sort -n -t '|' -k 1")
 
 (test-sad "by country"
           "cat density.txt | sad lines :2;\
            cat density.txt | sad lines 2: | sad split '|' | sad buffer -r |\
            sad sort -c 1 | sad format '~a|~9@a|~12@a|~11@a|~22@a~%'"
-          "head density.txt -n2 ; tail -10 density.txt | sort -t '|' -k 2")
+          "head -n2 density.txt ; tail -10 density.txt | sort -t '|' -k 2")
 
 (test-sad "by country reversed"
           "cat density.txt | sad lines :2;\
            cat density.txt | sad lines 2: | sad split '|' | sad buffer -r |\
            sad sort -r -c 1 | sad format '~a|~9@a|~12@a|~11@a|~22@a~%'"
-          "head density.txt -n2 ; tail -10 density.txt | sort -r -t '|' -k 2")
+          "head -n2 density.txt ; tail -10 density.txt | sort -r -t '|' -k 2")
 
 (test-sad "by area"
           "cat density.txt | sad lines :2;\
            cat density.txt | sad lines 2: | sad split '|' | sad buffer -r |\
            sad sort -C numeric -c 3 | sad format '~a|~9@a|~12@a|~11@a|~22@a~%'"
-          "head density.txt -n2 ; tail -10 density.txt | sort -n -t '|' -k 4")
+          "head -n2 density.txt ; tail -10 density.txt | sort -n -t '|' -k 4")
