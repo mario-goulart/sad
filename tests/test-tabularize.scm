2763160a45683dd801c9fa48394f3f5d7554065d
Mario Domenech Goulart <mario@parenteses.org>
2025-05-15 21:30:02 +0200

    test-tabularize: Add test for --borderless

--- tests/test-tabularize.scm
+++ tests/test-tabularize.scm
@@ -7,3 +7,12 @@
 └───────────┘
 "
           (run "seq 9 | sad buffer 3 | sad tabularize"))
+
+
+(test "tabularized seq"
+          "\
+1  2  3
+4  5  6
+7  8
+"
+          (run "seq 8 | sad buffer 3 | sad tabularize --borderless"))
